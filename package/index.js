394f07b2613facc79f12242aab19647949b49ca0
estrattonbailey <ericstrattonbailey@gmail.com>
2017-08-08 15:56:54 -0400

    Update micro-popover dep

--- package/index.js
+++ package/index.js
@@ -4,12 +4,15 @@ import {
   unmountComponentAtNode,
   findDOMNode
 } from 'react-dom'
-
 import Pop from 'micro-popover'
 
 export default class Popover extends React.Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      pinned: false
+    }
 
     this.actions = {
       pin: this.pin.bind(this),
@@ -33,7 +36,8 @@ export default class Popover extends React.Component {
       target: findDOMNode(this),
       popover: this.mount,
       position,
-      transitionSpeed
+      transitionSpeed,
+      onChange: state => this.setState({ pinned: state.pinned })
     })
   }
 
@@ -54,6 +58,9 @@ export default class Popover extends React.Component {
   }
 
   render () {
-    return this.props.children(this.actions)
+    return this.props.children({
+      ...this.actions,
+      pinned: this.state.pinned
+    })
   }
 }
